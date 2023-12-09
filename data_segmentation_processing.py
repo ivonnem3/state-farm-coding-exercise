d92b6a6be727582479407dc3bde4caebb69d3756
ivonnem3 <ivonneam0925@gmail.com>
2023-12-09 15:42:45 -0600

    code initial modularization

---
+++ data_segmentation_processing.py
@@ -0,0 +1,145 @@
+"""
+Description:
+"""
+# Load Libraries
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+def fix_special_char(data):
+    """
+    Description:
+    Input: pd.DataFrame
+    Output:
+    """
+    # Fix money and percents
+    data['x12'] = data['x12'].str.replace('$','')
+    data['x12'] = data['x12'].str.replace(',','')
+    data['x12'] = data['x12'].str.replace(')','')
+    data['x12'] = data['x12'].str.replace('(','-')
+    data['x12'] = data['x12'].astype(float)
+    data['x63'] = data['x63'].str.replace('%','')
+    data['x63'] = data['x63'].astype(float)
+
+    return data
+
+
+
+def create_dummies(data,data_imputed_std):
+    """
+    Description:
+    Input: pd.DataFrame
+    Output:
+    """
+    dumb5 = pd.get_dummies(data['x5'], drop_first=True, prefix='x5', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb5], axis=1, sort=False)
+
+    dumb31 = pd.get_dummies(data['x31'], drop_first=True, prefix='x31', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb31], axis=1, sort=False)
+
+    dumb81 = pd.get_dummies(data['x81'], drop_first=True, prefix='x81', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb81], axis=1, sort=False)
+
+    dumb82 = pd.get_dummies(data['x82'], drop_first=True, prefix='x82', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb82], axis=1, sort=False)
+
+    del dumb5, dumb31, dumb81, dumb82
+
+    return data_imputed_std
+
+
+
+def training_data_processing(data):
+    """
+    Description:
+    Input: pd.DataFrame
+    Output:
+    """
+    # Create a deep copy of dataframe
+    data = data.copy(deep=True)
+
+    # Fix money and percents
+    data = fix_special_char(data)
+
+    # Creating the train/val/test set
+    x_train, x_val, y_train, y_val = train_test_split(data.drop(columns=['y']), data['y'], test_size=0.1, random_state=13)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=4000, random_state=13)
+
+    # Concat features and target variables
+    train = pd.concat([x_train, y_train], axis=1, sort=False).reset_index(drop=True)
+    val = pd.concat([x_val, y_val], axis=1, sort=False).reset_index(drop=True)
+    test = pd.concat([x_test, y_test], axis=1, sort=False).reset_index(drop=True)
+
+    # With mean imputation from Train set
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+    std_scaler = StandardScaler()
+
+    # Prepare train dataset
+    train_imputed = pd.DataFrame(imputer.fit_transform(train.drop(columns=['y', 'x5', 'x31',  'x81' ,'x82'])), columns=train.drop(columns=['y', 'x5', 'x31', 'x81', 'x82']).columns)
+    train_imputed_std = pd.DataFrame(std_scaler.fit_transform(train_imputed), columns=train_imputed.columns)
+
+    train_imputed_std = create_dummies(train,train_imputed_std)
+    train_imputed_std = pd.concat([train_imputed_std, train['y']], axis=1, sort=False)
+
+    # Prepare validation dataset
+    val_imputed = pd.DataFrame(imputer.transform(val.drop(columns=['y', 'x5', 'x31', 'x81' ,'x82'])), columns=train.drop(columns=['y','x5', 'x31', 'x81', 'x82']).columns)
+    val_imputed_std = pd.DataFrame(std_scaler.transform(val_imputed), columns=train_imputed.columns)
+
+    val_imputed_std = create_dummies(val,val_imputed_std)
+    val_imputed_std = pd.concat([val_imputed_std, val['y']], axis=1, sort=False)
+
+    # Prepare Test Set
+    test_imputed = pd.DataFrame(imputer.transform(test.drop(columns=['y', 'x5', 'x31', 'x81' ,'x82'])), columns=train.drop(columns=['y','x5', 'x31', 'x81', 'x82']).columns)
+    test_imputed_std = pd.DataFrame(std_scaler.transform(test_imputed), columns=train_imputed.columns)
+
+    test_imputed_std = create_dummies(test,test_imputed_std)
+    test_imputed_std = pd.concat([test_imputed_std, test['y']], axis=1, sort=False)
+
+    return train_imputed_std, val_imputed_std, test_imputed_std
+
+
+
+def prediction_data_processing(data):
+    """
+    Description:
+    Input: pd.DataFrame
+    Output:
+    """
+    # Create a deep copy of dataframe
+    data = data.copy(deep=True)
+
+    # Fix money and percents
+    data = fix_special_char(data)
+
+    # Impute data
+    imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+    data_imputed = pd.DataFrame(imputer.fit_transform(data.drop(columns=['x5', 'x31',  'x81' ,'x82'])), columns=data.drop(columns=[ 'x5', 'x31', 'x81', 'x82']).columns)
+    std_scaler = StandardScaler()
+    data_imputed_std = pd.DataFrame(std_scaler.fit_transform(data_imputed), columns=data_imputed.columns)
+
+    # Create Dummies
+    dumb5 = pd.get_dummies(data['x5'], drop_first=True, prefix='x5', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb5], axis=1, sort=False)
+
+    dumb31 = pd.get_dummies(data['x31'], drop_first=True, prefix='x31', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb31], axis=1, sort=False)
+
+    dumb81 = pd.get_dummies(data['x81'], drop_first=True, prefix='x81', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb81], axis=1, sort=False)
+
+    dumb82 = pd.get_dummies(data['x82'], drop_first=True, prefix='x82', prefix_sep='_', dummy_na=True)
+    data_imputed_std = pd.concat([data_imputed_std, dumb82], axis=1, sort=False)
+
+    del dumb5, dumb31, dumb81, dumb82
+
+    # Select Variables
+    variables = ['x5_saturday','x81_July','x81_December','x31_japan','x81_October',
+                 'x5_sunday','x31_asia','x81_February','x91','x81_May','x5_monday','x81_September',
+                 'x81_March','x53','x81_November','x44','x81_June','x12','x5_tuesday','x81_August',
+                 'x81_January','x62','x31_germany','x58','x56']
+
+    data_imputed_std = data_imputed_std[variables]
+
+    return data_imputed_std
--- glm_model.py
+++ glm_model.py
@@ -16,64 +16,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 import statsmodels.api as sm
 import pickle
+from data_segmentation_processing import training_data_processing
 
 ## Local Path Set up
 path = os.getcwd()
 os.chdir('state-farm-coding-exercise')
 
-## Load Train & Test data
-#-------------------------------------------
+## Load Model Train data
 raw_train = pd.read_csv('exercise_26_train.csv')
-raw_test = pd.read_csv('exercise_26_test.csv')
 
-
-## Feature Engineering
-#-------------------------------------------
-train_val = raw_train.copy(deep=True)
-
-# Fixing the money and percents#
-train_val['x12'] = train_val['x12'].str.replace('$','')
-train_val['x12'] = train_val['x12'].str.replace(',','')
-train_val['x12'] = train_val['x12'].str.replace(')','')
-train_val['x12'] = train_val['x12'].str.replace('(','-')
-train_val['x12'] = train_val['x12'].astype(float)
-train_val['x63'] = train_val['x63'].str.replace('%','')
-train_val['x63'] = train_val['x63'].astype(float)
-
-# Creating the train/val/test set
-x_train, x_val, y_train, y_val = train_test_split(train_val.drop(columns=['y']), train_val['y'], test_size=0.1, random_state=13)
-x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=4000, random_state=13)
-
-# Smashing sets back together
-train = pd.concat([x_train, y_train], axis=1, sort=False).reset_index(drop=True)
-val = pd.concat([x_val, y_val], axis=1, sort=False).reset_index(drop=True)
-test = pd.concat([x_test, y_test], axis=1, sort=False).reset_index(drop=True)
-
-# With mean imputation from Train set
-imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
-train_imputed = pd.DataFrame(imputer.fit_transform(train.drop(columns=['y', 'x5', 'x31',  'x81' ,'x82'])), columns=train.drop(columns=['y', 'x5', 'x31', 'x81', 'x82']).columns)
-std_scaler = StandardScaler()
-train_imputed_std = pd.DataFrame(std_scaler.fit_transform(train_imputed), columns=train_imputed.columns)
-
-# Create dummies
-dumb5 = pd.get_dummies(train['x5'], drop_first=True, prefix='x5', prefix_sep='_', dummy_na=True)
-train_imputed_std = pd.concat([train_imputed_std, dumb5], axis=1, sort=False)
-
-dumb31 = pd.get_dummies(train['x31'], drop_first=True, prefix='x31', prefix_sep='_', dummy_na=True)
-train_imputed_std = pd.concat([train_imputed_std, dumb31], axis=1, sort=False)
-
-dumb81 = pd.get_dummies(train['x81'], drop_first=True, prefix='x81', prefix_sep='_', dummy_na=True)
-train_imputed_std = pd.concat([train_imputed_std, dumb81], axis=1, sort=False)
-
-dumb82 = pd.get_dummies(train['x82'], drop_first=True, prefix='x82', prefix_sep='_', dummy_na=True)
-train_imputed_std = pd.concat([train_imputed_std, dumb82], axis=1, sort=False)
-train_imputed_std = pd.concat([train_imputed_std, train['y']], axis=1, sort=False)
-
-del dumb5, dumb31, dumb81, dumb82
-
+## Data Processing & Feature Engineering
+train_imputed_std, val_imputed_std, test_imputed_std = training_data_processing(raw_train)
 
 ## Initial Feature Selection
-#-------------------------------------------
 exploratory_LR = LogisticRegression(penalty='l1', fit_intercept=False, solver='liblinear')
 exploratory_LR.fit(train_imputed_std.drop(columns=['y']), train_imputed_std['y'])
 exploratory_results = pd.DataFrame(train_imputed_std.drop(columns=['y']).columns).rename(columns={0:'name'})
@@ -83,58 +38,16 @@ var_reduced = exploratory_results.nlargest(25,'coefs_squared')
 
 
 ## Preliminary Model
-#-------------------------------------------
 variables = var_reduced['name'].to_list()
 train_y = train_imputed_std['y']
 train_X = train_imputed_std[variables].copy()
 train_X.replace({False:0, True:1}, inplace = True)
 logit = sm.Logit(train_y, train_X)
 
-# fit the model
+## Fit Model
 result = logit.fit()
 
-## Prepping the validation set
-#-------------------------------------------
-val_imputed = pd.DataFrame(imputer.transform(val.drop(columns=['y', 'x5', 'x31', 'x81' ,'x82'])), columns=train.drop(columns=['y','x5', 'x31', 'x81', 'x82']).columns)
-val_imputed_std = pd.DataFrame(std_scaler.transform(val_imputed), columns=train_imputed.columns)
-
-dumb5 = pd.get_dummies(val['x5'], drop_first=True, prefix='x5', prefix_sep='_', dummy_na=True)
-val_imputed_std = pd.concat([val_imputed_std, dumb5], axis=1, sort=False)
-
-dumb31 = pd.get_dummies(val['x31'], drop_first=True, prefix='x31', prefix_sep='_', dummy_na=True)
-val_imputed_std = pd.concat([val_imputed_std, dumb31], axis=1, sort=False)
-
-dumb81 = pd.get_dummies(val['x81'], drop_first=True, prefix='x81', prefix_sep='_', dummy_na=True)
-val_imputed_std = pd.concat([val_imputed_std, dumb81], axis=1, sort=False)
-
-dumb82 = pd.get_dummies(val['x82'], drop_first=True, prefix='x82', prefix_sep='_', dummy_na=True)
-val_imputed_std = pd.concat([val_imputed_std, dumb82], axis=1, sort=False)
-val_imputed_std = pd.concat([val_imputed_std, val['y']], axis=1, sort=False)
-
-
-## Prepping the test set
-#-------------------------------------------
-test_imputed = pd.DataFrame(imputer.transform(test.drop(columns=['y', 'x5', 'x31', 'x81' ,'x82'])), columns=train.drop(columns=['y','x5', 'x31', 'x81', 'x82']).columns)
-test_imputed_std = pd.DataFrame(std_scaler.transform(test_imputed), columns=train_imputed.columns)
-
-# 3 create dummies
-
-dumb5 = pd.get_dummies(test['x5'], drop_first=True, prefix='x5', prefix_sep='_', dummy_na=True)
-test_imputed_std = pd.concat([test_imputed_std, dumb5], axis=1, sort=False)
-
-dumb31 = pd.get_dummies(test['x31'], drop_first=True, prefix='x31', prefix_sep='_', dummy_na=True)
-test_imputed_std = pd.concat([test_imputed_std, dumb31], axis=1, sort=False)
-
-dumb81 = pd.get_dummies(test['x81'], drop_first=True, prefix='x81', prefix_sep='_', dummy_na=True)
-test_imputed_std = pd.concat([test_imputed_std, dumb81], axis=1, sort=False)
-
-dumb82 = pd.get_dummies(test['x82'], drop_first=True, prefix='x82', prefix_sep='_', dummy_na=True)
-test_imputed_std = pd.concat([test_imputed_std, dumb82], axis=1, sort=False)
-test_imputed_std = pd.concat([test_imputed_std, test['y']], axis=1, sort=False)
-
-
 ## Finaliz the Model
-#-------------------------------------------
 train_and_val = pd.concat([train_imputed_std, val_imputed_std])
 all_train = pd.concat([train_and_val, test_imputed_std])
 variables = var_reduced['name'].to_list()
@@ -143,10 +56,9 @@ all_train_X = all_train[variables].copy()
 all_train_X.replace({False:0, True:1}, inplace = True)
 final_logit = sm.Logit(all_train_y, all_train_X)
 
-# fit the model
+## Final Model Fit
 final_result = final_logit.fit()
 
-
-# Save model
+# Pickle model
 with open('glm_model.pickle', 'wb') as f:
     pickle.dump(final_result, f)
