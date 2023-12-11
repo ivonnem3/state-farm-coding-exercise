2bca5095c186d968d5aea562de1859d087d20e45
ivonnem3 <ivonneam0925@gmail.com>
2023-12-11 12:34:38 -0600

    added 3rd api & edited readme file

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # state-farm-coding-exercise
 ***This project is a coding classification interview exercise for a Machine Learning position at State Farm.***
 
-![localhost_main page](state-farm-coding-exercise\localhost_1313_screenshot.png)
+### Quick API Examples
+
+| API Endpoint   | Action | Description        |
+| -------------  | ------- |-------------      |
+| /localhost1313 | `GET`   |This is the main api page, were you can get information   about the api calls |
+|/localhost1313/predict | `POST` | This API allows you to feed in your variables and obtain the predictions and probabilities if the prediction is >= 0.75
+|/localhost1313/predict_all | `POST` | This allows you to get all of the predictions for all of your input data |
+
 
 ### Installation
 Download the project from the github repo and unzip the file. Open up a terminal and navigate to the project folder. You can then install all the requirements by typing in the following command:
@@ -10,9 +17,23 @@ Download the project from the github repo and unzip the file. Open up a terminal
 pip install -r requirements.txt
 ```
 
-### Quick API Examples
-[fill in]
+Note also make sure that you have python version python 3.7.1 or greater. Here is how you can chech your current version:
+```
+python --version
+```
+
+If your not you can use this command to upgrade in your Windows machine:
+
+```
+py -m pip install --upgrade pip
+```
+
+or Linux/macOS:
+
+```
+brew upgrade python
+```
 
 
-### User Interface
-[fill in]
+
+![localhost_main page](localhost_1313_screenshot.png)
--- api.py
+++ api.py
@@ -23,7 +23,7 @@ def home():
     # RENDER TEMPLATE
     return render_template('index.html')
 
-# Route for prediction using the GLM model
+# Route for prediction using the GLM model with 0.75 prediction
 @app.route('/predict', methods=['POST'])
 def predict():
     """
@@ -58,6 +58,50 @@ def predict():
         # Filter prediction for 75th percentile & arrange in alphabetical order
         predicted_outcome = predicted_outcome.reindex(sorted(predicted_outcome.columns), axis=1)
         predicted_outcome = predicted_outcome[predicted_outcome['business_outcome'] >= 0.75]
+
+        # Convert DataFrame to a list of dictionaries
+        result_list = predicted_outcome.to_dict(orient='records')
+
+        # Return the predictions as json
+        return jsonify(result_list)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# Route for prediction using the GLM model with 0.75 prediction
+@app.route('/predict_all', methods=['POST'])
+def predict_all():
+    """
+    Description:
+        This funtion is the main api route for obtaining our predictions
+    """
+    try:
+        # Assuming the input data is in JSON format
+        input_data = request.json
+
+        if not input_data:
+            # If there is no input data, return an empty list
+            return jsonify([])
+
+        # Make predictions using the loaded model
+        with open('glm_model.pickle', 'rb') as file:
+            glm_model = pickle.load(file)
+
+        # Format input data using data_formating
+        predicted_outcome = data_formating(input_data)
+
+        # Calculate Predictions (Unit test length is > 1 for probs)
+        predicted_outcome['business_outcome'] = glm_model.predict(predicted_outcome)
+
+        # Calculate Probabilities
+        """
+        Note: If the data is one entry, it will return nan.
+        Was not sure if to fix this by implemening the already know phat
+        """
+        predicted_outcome['phat'] = pd.qcut(predicted_outcome['business_outcome'], q=[0, .25, .5, .75, 1.], duplicates='drop').astype(str)
+
+        # Filter prediction for 75th percentile & arrange in alphabetical order
+        predicted_outcome = predicted_outcome.reindex(sorted(predicted_outcome.columns), axis=1)
 
         # Convert DataFrame to a list of dictionaries
         result_list = predicted_outcome.to_dict(orient='records')
